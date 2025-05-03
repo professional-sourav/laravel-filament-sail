91e59671e5dd21aff77408eca0b2045c4f80f263
SouravCh <professional.sourav.com>
2025-05-03 18:31:53 +0000

    Added page's options

--- app/Filament/Pages/ExamplePage.php
+++ app/Filament/Pages/ExamplePage.php
@@ -2,7 +2,9 @@
 
 namespace App\Filament\Pages;
 
+use Filament\Actions\Action;
 use Filament\Pages\Page;
+use Filament\Support\Enums\MaxWidth;
 
 class ExamplePage extends Page
 {
@@ -12,8 +14,35 @@ class ExamplePage extends Page
 
     protected static ?string $title = 'My Example Page';
 
+    public $defaultAction = 'onboarding';
+
     public static function canAccess(): bool
     {
         return auth()->user()->canManageExamplePage();
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('Create')
+                ->label('Create Example')
+                ->url(static::getUrl(['action' => 'create']))
+                ->icon('heroicon-o-plus')
+                ->color('primary')
+                ->requiresConfirmation()
+                ->action(fn () => $this->notify('success', 'Example created successfully!')),
+        ];
+    }
+
+    public function onboardingAction(): Action
+    {
+        return Action::make('onboarding')
+            ->modalHeading('Welcome')
+            ->visible(fn (): bool => true);
+    }
+
+    public function getMaxContentWidth(): MaxWidth
+    {
+        return MaxWidth::Full;
     }
 }
